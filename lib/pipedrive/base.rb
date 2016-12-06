80f3a76c849a48b307df0d4cc22e64ab771d0079
Mike Szyndel <hajder@users.noreply.github.com>
2016-12-06 19:31:34 +0100

    Remove unnecessary puts

--- lib/pipedrive/base.rb
+++ lib/pipedrive/base.rb
@@ -78,7 +78,6 @@ module Pipedrive
       #
       # @param [HTTParty::Response] response
       def bad_response(response, params={})
-        puts params.inspect
         if response.class == HTTParty::Response
           raise HTTParty::ResponseError, response
         end
